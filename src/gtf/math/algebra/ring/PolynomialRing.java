4240409a789cdadb32e46b092e70b19723446354
Top J. Quark <37160280+topquark22@users.noreply.github.com>
2026-05-15 12:38:22 -0400

    Update PolynomialRing for current SparseVector API

--- src/gtf/math/algebra/ring/PolynomialRing.java
+++ src/gtf/math/algebra/ring/PolynomialRing.java
@@ -1,7 +1,5 @@
 package gtf.math.algebra.ring;
 
-import java.math.BigInteger;
-
 import gtf.math.algebra.Ring;
 import gtf.math.matrix.SparseVector;
 
@@ -17,28 +15,36 @@ import gtf.math.matrix.SparseVector;
  * @param <T> Type of elements in the base ring
  * @param <R> the base ring
  */
-public class PolynomialRing<T, R extends Ring<T>> implements Ring<PolynomialRing<T, R>.Polynomial> {
+public class PolynomialRing<T, R extends Ring<T>>
+    implements Ring<PolynomialRing<T, R>.Polynomial> {
 
   public class Polynomial {
-    private final SparseVector<BigInteger, T, R> coefficients;
+    private final SparseVector<T, R> coefficients;
+
     public Polynomial() {
-      coefficients = new SparseVector<BigInteger, T, R>(ring);
+      coefficients = new SparseVector<T, R>(ring, 0);
     }
+
     /**
      * copy constructor
      * @param orig
      */
     public Polynomial(Polynomial orig) {
-      coefficients = new SparseVector<BigInteger, T, R>(orig.coefficients);
+      coefficients = new SparseVector<T, R>(orig.coefficients);
     }
+
     /**
-     * Make a new polynomial from an old one by multiplying it by x^exp * scalar (on the left).
-     * @param n
+     * Make a new polynomial from an old one by multiplying it by
+     * x^exp * scalar on the left.
+     *
+     * @param orig the original polynomial
+     * @param exp the exponent shift
+     * @param scalar the scalar multiplier
      */
-    private Polynomial(Polynomial orig, BigInteger exp, T scalar) {
-      coefficients = new SparseVector<BigInteger, T, R>(ring);
-      for (BigInteger i : orig.coefficients) {
-        coefficients.set(i.add(exp), ring.mul(scalar, orig.coefficients.get(i)));
+    private Polynomial(Polynomial orig, int exp, T scalar) {
+      coefficients = new SparseVector<T, R>(ring, 0);
+      for (int i : orig.coefficients) {
+        coefficients.set(i + exp, ring.mul(scalar, orig.coefficients.get(i)));
       }
     }
   }
@@ -51,41 +57,42 @@ public class PolynomialRing<T, R extends Ring<T>> implements Ring<PolynomialRing
     this.ring = ring;
     zero = new Polynomial();
     one = new Polynomial();
-    one.coefficients.set(BigInteger.ONE, ring.id());
+    one.coefficients.set(0, ring.id());
   }
 
-  //@Override
+  @Override
   public Polynomial zero() {
     return zero;
   }
   
-  //@Override
+  @Override
   public Polynomial id() {
     return one;
   }
 
-  //@Override
+  @Override
   public Polynomial add(Polynomial arg1, Polynomial arg2) {
     Polynomial result = new Polynomial(arg1);
-    for (BigInteger i : arg2.coefficients) {
-      result.coefficients.set(i, ring.add(result.coefficients.get(i), arg2.coefficients.get(i)));
+    for (int i : arg2.coefficients) {
+      result.coefficients.set(i,
+          ring.add(result.coefficients.get(i), arg2.coefficients.get(i)));
     }
     return result;
   }
 
-  //@Override
+  @Override
   public Polynomial neg(Polynomial arg) {
     Polynomial result = new Polynomial();
-    for (BigInteger i : arg.coefficients) {
+    for (int i : arg.coefficients) {
       result.coefficients.set(i, ring.neg(arg.coefficients.get(i)));
     }
     return result;
   }
 
-  //@Override
+  @Override
   public Polynomial mul(Polynomial arg1, Polynomial arg2) {
     Polynomial result = new Polynomial();
-    for (BigInteger i : arg1.coefficients) {
+    for (int i : arg1.coefficients) {
       Polynomial p = new Polynomial(arg2, i, arg1.coefficients.get(i));
       result = add(result, p);
     }
@@ -95,7 +102,7 @@ public class PolynomialRing<T, R extends Ring<T>> implements Ring<PolynomialRing
   /**
    * This ring is a module over itself.
    */
-  //@Override
+  @Override
   public Ring<Polynomial> ring() {
     return this;
   }
