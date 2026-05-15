1c595027b6a213cf87507d443a91b0645a37a8ab
Top J. Quark <37160280+topquark22@users.noreply.github.com>
2026-05-15 16:22:17 -0400

    Move ContinuousDistribution to stats distribution package

---
+++ src/gtf/math/stats/distribution/ContinuousDistribution.java
@@ -0,0 +1,25 @@
+package gtf.math.stats.distribution;
+
+/**
+ * Represents a continuous probability distribution.
+ *
+ * @author gtf
+ */
+public interface ContinuousDistribution {
+
+  /**
+   * Evaluates the probability density function.
+   *
+   * @param x the evaluation point
+   * @return the density at x
+   */
+  double density(double x);
+
+  /**
+   * Evaluates the cumulative distribution function.
+   *
+   * @param x the evaluation point
+   * @return P(X <= x)
+   */
+  double cumulative(double x);
+}
