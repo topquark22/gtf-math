93dd4fdf90f8d5bd77276f4e3412dfdf6bd1677d
Top J. Quark <37160280+topquark22@users.noreply.github.com>
2026-05-15 10:46:50 -0400

    Move InnerProductSpace to geometry package

---
+++ src/gtf/math/geometry/InnerProductSpace.java
@@ -0,0 +1,47 @@
+package gtf.math.geometry;
+
+import gtf.math.algebra.Field;
+import gtf.math.algebra.FiniteDimensionalVectorSpace;
+
+
+/**
+ * A finite-dimensional inner-product space over a field.
+ *
+ * <p>
+ * This interface extends a finite-dimensional vector space with a specified
+ * inner product.
+ * </p>
+ *
+ * @author gtf
+ *
+ * @param <V> the type of the vector-space elements
+ * @param <S> the type of the scalar-field elements
+ * @param <F> the scalar field
+ */
+public interface InnerProductSpace<V, S, F extends Field<S>>
+    extends FiniteDimensionalVectorSpace<V, S, F> {
+
+  /**
+   * Computes the inner product of two vectors.
+   *
+   * <p>
+   * The default implementation computes the standard coordinate-wise inner
+   * product relative to the distinguished basis.
+   * </p>
+   *
+   * @param v1 the first vector
+   * @param v2 the second vector
+   * @return the inner product
+   */
+  default S innerProduct(V v1, V v2) {
+    F field = ring();
+    S sum = field.zero();
+
+    for (int i = 0; i < dimension(); i++) {
+      sum = field.add(sum,
+          field.mul(coordinate(v1, i), coordinate(v2, i)));
+    }
+
+    return sum;
+  }
+}
