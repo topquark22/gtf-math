b00bf022bc8e7239e940f2361abc49caecf4a52b
Top J. Quark <37160280+topquark22@users.noreply.github.com>
2026-05-15 12:38:46 -0400

    Update MatrixReader to use MatrixImpl

--- src/gtf/math/matrix/MatrixReader.java
+++ src/gtf/math/matrix/MatrixReader.java
@@ -7,6 +7,7 @@ import java.io.BufferedReader;
 import java.util.StringTokenizer;
 
 import gtf.math.BigFraction;
+import gtf.math.algebra.field.Q;
 
 /**
  * Reads matrices from an input stream.
@@ -35,41 +36,60 @@ class MatrixReader {
    *@exception MatrixFormatException  Description of the Exception
    *@exception IOException            Description of the Exception
    */
-  public static Matrix read(InputStream in) throws MatrixFormatException, IOException {
-    Matrix matrix;
+  public static Matrix<BigFraction, Q> read(InputStream in)
+      throws MatrixFormatException, IOException {
+
+    Matrix<BigFraction, Q> matrix;
     int rows;
     int cols;
     BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
     try {
       // read the dimensions from the first line
       String line = reader.readLine();
+
       if (line != null) {
         try {
           StringTokenizer tokenizer = new StringTokenizer(line);
+
           if (tokenizer.countTokens() != 2) {
-            throw new MatrixFormatException("could not read the rows/columns spec");
+            throw new MatrixFormatException(
+                "could not read the rows/columns spec");
           }
+
           String rowsStr = tokenizer.nextToken();
           rows = Integer.parseInt(rowsStr);
+
           String colsStr = tokenizer.nextToken();
           cols = Integer.parseInt(colsStr);
+
         } catch (NumberFormatException e) {
           throw new MatrixFormatException("Bad dimensions");
         }
       } else {
         throw new MatrixFormatException("Couldn't read dimensions");
       }
+
       if (rows <= 0 || cols <= 0) {
         throw new IllegalArgumentException("Bad dimensions");
       }
-      matrix = new Matrix(rows, cols);
+
+      matrix = new MatrixImpl<BigFraction, Q>(
+          new Q(),
+          rows,
+          cols,
+          new ArrayStorageFactory<BigFraction>());
+
       int row = 0;
-      line = reader.readLine();
+      String line = reader.readLine();
+
       while (row < rows && line != null) {
         StringTokenizer tokenizer = new StringTokenizer(line);
         int col = 0;
+
         while (col < cols && tokenizer.hasMoreTokens()) {
           String token = tokenizer.nextToken();
+
           try {
             matrix.setCell(row, col, new BigFraction(token));
           } catch (NumberFormatException e) {
@@ -77,21 +97,26 @@ class MatrixReader {
           } catch (IllegalArgumentException e) {
             throw new MatrixFormatException(e.getMessage(), row, col);
           }
+
           col++;
         }
+
         if (col < cols) {
           throw new MatrixFormatException("Not enough values", row);
         }
+
         row++;
         line = reader.readLine();
       }
+
       if (row < rows) {
         throw new MatrixFormatException("Not enough rows", row);
       }
+
     } finally {
       reader.close();
     }
+
     return matrix;
   }
 }
-
