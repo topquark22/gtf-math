66e063dce2cc53f39134aed0e991ffac0ad23ac8
Top J. Quark <37160280+topquark22@users.noreply.github.com>
2026-05-15 09:17:48 -0400

    Add coordinate access to finite-dimensional vector spaces

--- src/gtf/math/algebra/FiniteDimensionalVectorSpace.java
+++ src/gtf/math/algebra/FiniteDimensionalVectorSpace.java
@@ -7,7 +7,8 @@ package gtf.math.algebra;
  * <p>
  * This interface adds the concept of dimension and a distinguished basis to
  * the general notion of a vector space. The choice of basis is part of the
- * structure exposed by this interface.
+ * structure exposed by this interface. Coordinates are therefore interpreted
+ * relative to that distinguished basis.
  * </p>
  *
  * @author gtf
@@ -31,4 +32,13 @@ public interface FiniteDimensionalVectorSpace<V, S, F extends Field<S>>
    * @return the basis vector at the given index
    */
   V basisVector(int index);
+
+  /**
+   * Returns one coordinate of a vector relative to the distinguished basis.
+   *
+   * @param vector the vector
+   * @param index the coordinate index
+   * @return the coordinate value
+   */
+  S coordinate(V vector, int index);
 }
