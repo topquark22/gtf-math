3deab3f22b10fcce80c5c997d58740ecdf82622f
Top J. Quark <37160280+topquark22@users.noreply.github.com>
2026-05-15 09:17:58 -0400

    Add default inner-product implementation

--- src/gtf/math/algebra/InnerProductSpace.java
+++ src/gtf/math/algebra/InnerProductSpace.java
@@ -21,9 +21,24 @@ public interface InnerProductSpace<V, S, F extends Field<S>>
   /**
    * Computes the inner product of two vectors.
    *
+   * <p>
+   * The default implementation computes the standard coordinate-wise inner
+   * product relative to the distinguished basis.
+   * </p>
+   *
    * @param v1 the first vector
    * @param v2 the second vector
    * @return the inner product
    */
-  S innerProduct(V v1, V v2);
+  default S innerProduct(V v1, V v2) {
+    F field = ring();
+    S sum = field.zero();
+
+    for (int i = 0; i < dimension(); i++) {
+      sum = field.add(sum,
+          field.mul(coordinate(v1, i), coordinate(v2, i)));
+    }
+
+    return sum;
+  }
 }
