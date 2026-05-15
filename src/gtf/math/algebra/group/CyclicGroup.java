cbe3f96f0f3c884b91a99405ac15ebafa0a1ac36
Top J. Quark <37160280+topquark22@users.noreply.github.com>
2026-05-15 09:41:11 -0400

    Deprecate algebra.group.CyclicGroup

--- src/gtf/math/algebra/group/CyclicGroup.java
+++ src/gtf/math/algebra/group/CyclicGroup.java
@@ -2,35 +2,14 @@ package gtf.math.algebra.group;
 
 import java.math.BigInteger;
 
-import gtf.math.algebra.Group;
-
 
 /**
- * Cyclic group of order n, written multiplicatively.
- * Implemented additively using BigInteger.
- * 
- * @author gtf
+ * @deprecated Use {@link gtf.math.group.CyclicGroup} instead.
  */
-public final class CyclicGroup implements Group<BigInteger> {
+@Deprecated
+public class CyclicGroup extends gtf.math.group.CyclicGroup {
 
-  private final BigInteger modulus;
-  
   public CyclicGroup(BigInteger order) {
-    if (order.compareTo(BigInteger.ONE) < 0) {
-      throw new IllegalArgumentException("order must be >= 1");
-    }
-    modulus = order;
-  }
-  
-  public BigInteger inv(BigInteger arg) {
-    return arg.negate().mod(modulus);
-  }
-
-  public BigInteger mul(BigInteger arg1, BigInteger arg2) {
-    return arg1.add(arg2).mod(modulus);
-  }
-
-  public BigInteger id() {
-    return BigInteger.ZERO;
+    super(order);
   }
 }
