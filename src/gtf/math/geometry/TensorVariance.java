ead9e2aab1a94fc46fbf6af75fa019ca48bce470
Top J. Quark <37160280+topquark22@users.noreply.github.com>
2026-05-15 12:17:27 -0400

    Add tensor variance enum

---
+++ src/gtf/math/geometry/TensorVariance.java
@@ -0,0 +1,21 @@
+package gtf.math.geometry;
+
+
+/**
+ * Variance of a tensor index.
+ *
+ * <p>
+ * A contravariant index is conventionally written as an upper index, while a
+ * covariant index is conventionally written as a lower index.
+ * </p>
+ *
+ * @author gtf
+ */
+public enum TensorVariance {
+
+  /** Upper-index variance. */
+  CONTRAVARIANT,
+
+  /** Lower-index variance. */
+  COVARIANT
+}
