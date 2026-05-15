370d65268597611b977867c2e0e33c2847d2f2fb
Top J. Quark <37160280+topquark22@users.noreply.github.com>
2026-05-15 09:35:00 -0400

    Deprecate algebra.AbelianGroup in favor of group.AbelianGroup

--- src/gtf/math/algebra/AbelianGroup.java
+++ src/gtf/math/algebra/AbelianGroup.java
@@ -2,33 +2,9 @@ package gtf.math.algebra;
 
 
 /**
- * Defines operations on an abstract abelian group, written additively.
- * 
- * @param <T> The type used for the group elements. Must have a meaningful
- * equals(), hashCode() methods.
- * 
- * @author gtf
+ * @deprecated Use {@link gtf.math.group.AbelianGroup} instead.
  */
-public interface AbelianGroup<T> {
-
-  /**
-   * Returns the zero element.
-   * 
-   * @return
-   */
-  T zero();
-  
-  /**
-   * Returns the sum of two elements.
-   *    
-   * @param arg1
-   * @param arg2
-   * @return
-   */
-  T add(T arg1, T arg2);
-  
-  /**
-   * Returns the additive inverse of an element.
-   */
-  T neg(T arg);
+@Deprecated
+public interface AbelianGroup<T>
+    extends gtf.math.group.AbelianGroup<T> {
 }
