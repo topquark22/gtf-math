d7da48cdf0421fa4ebc45a90984af5f4f1439350
Top J. Quark <37160280+topquark22@users.noreply.github.com>
2026-05-15 20:03:04 -0400

    Simplify ZmodP and strengthen primality verification

--- src/gtf/math/algebra/field/ZmodP.java
+++ src/gtf/math/algebra/field/ZmodP.java
@@ -14,46 +14,23 @@ import gtf.math.algebra.ring.ZmodN;
 public class ZmodP extends ZmodN implements Field<BigInteger> {
 
   /**
-   * Certainty for argument checking purposes only. If caller passes
-   * an order that is not prime, there is a 1 in a million chance
-   * that this won't be detected, which is extremely unlikely
-   * to cause a problem, considering that it is only a sanity
-   * check in the first place.
+   * Certainty for argument checking purposes only.
    */
-  private static final int PRIME_CERTAINTY = 20;
-  
-  private final BigInteger modulus;
+  private static final int PRIME_CERTAINTY = 100;
   
   public ZmodP(BigInteger order) {
     super(order);
     if (!order.isProbablePrime(PRIME_CERTAINTY)) {
       throw new IllegalArgumentException("order must be prime");
     }
-    modulus = order;
   }
   
+  @Override
   public BigInteger inv(BigInteger arg) {
-    return arg.modInverse(modulus);
-  }
-
-  public BigInteger mul(BigInteger arg1, BigInteger arg2) {
-    // TODO efficient modular multiplication using bit shift
-    return arg1.multiply(arg2).mod(modulus);
-  }
-
-  public BigInteger id() {
-    return BigInteger.ONE;
-  }
-
-  public BigInteger zero() {
-    return BigInteger.ZERO;
-  }
-
-  public BigInteger add(BigInteger arg1, BigInteger arg2) {
-    return arg1.add(arg2).mod(modulus);
-  }
-
-  public BigInteger neg(BigInteger arg) {
-    return arg.negate().mod(modulus);
+    BigInteger value = arg.mod(modulus());
+    if (value.signum() == 0) {
+      throw new ArithmeticException("zero has no multiplicative inverse");
+    }
+    return value.modInverse(modulus());
   }
 }
