c068723a9268cb84174dceaa0f74443a3372900d
Top J. Quark <37160280+topquark22@users.noreply.github.com>
2026-05-15 20:13:07 -0400

    Add polynomial storage model abstraction

---
+++ src/gtf/math/algebra/polynomial/PolynomialStorageModel.java
@@ -0,0 +1,37 @@
+package gtf.math.algebra.polynomial;
+
+
+/**
+ * Represents the storage backing for polynomial coefficients.
+ *
+ * @param <T> coefficient type
+ *
+ * @author gtf
+ */
+interface PolynomialStorageModel<T> {
+
+  /**
+   * Returns the coefficient of x^degree.
+   *
+   * @param degree the degree
+   * @return the coefficient
+   */
+  T coefficient(int degree);
+
+  /**
+   * Returns the largest degree having a stored nonzero coefficient,
+   * or -1 for the zero polynomial.
+   *
+   * @return the degree
+   */
+  int degree();
+
+  /**
+   * Returns the next degree after the given one having a stored
+   * nonzero coefficient.
+   *
+   * @param degree the current degree
+   * @return the next stored degree, or -1 if none exists
+   */
+  int nextDegree(int degree);
+}
