c5bda1994961e05fcbe03a27c73523b3ae9d5463
Top J. Quark <37160280+topquark22@users.noreply.github.com>
2026-05-15 22:38:29 -0400

    Update ArrayStorageFactory.java

--- src/gtf/math/matrix/ArrayStorageFactory.java
+++ src/gtf/math/matrix/ArrayStorageFactory.java
@@ -8,7 +8,7 @@ package gtf.math.matrix;
  *
  * @param <T> the type of the matrix elements
  */
-public class ArrayStorageFactory<T> implements StorageFactory<T> {
+class ArrayStorageFactory<T> implements StorageFactory<T> {
 
   @Override
   public StorageModel<T> createStorage(int rows, int cols, T zero) {
